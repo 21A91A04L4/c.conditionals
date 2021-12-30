0ca2826cd3c1bc82af7eb78f4263d8b6816fbe74
Sudhakommireddy <96324729+Sudhakommireddy@users.noreply.github.com>
2021-12-30 15:41:41 +0530

    Add files via upload

---
+++ profitloss.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{ 
+    int cp,sp;
+    printf ("enter two numbers");
+    scanf("%d%d",&cp,&sp);
+    if(cp<sp)
+    {
+	
+    printf("profit");
+    }
+    else if(cp>sp)
+    {
+	 printf("loss");
+    }
+    else
+    {
+	 printf("zero");
+    }
+}
