da99fbb6aa7079b54733cf316006b24ba09d4b35
Sudhakommireddy <96324729+Sudhakommireddy@users.noreply.github.com>
2021-12-31 15:05:54 +0530

    Add files via upload

---
+++ failedmarks.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int m,p,c,cs;
+	scanf("%d%d%d%d",&m,&p,&c,&cs);
+	if(m<35)
+	{
+	printf("failed in maths");
+    }
+    if(p<35)
+    {
+    printf("failed in physics");
+	}
+	if(c<35)
+	{
+    printf("\nfailed in chemistry");
+    }
+    if(cs<35)
+    {
+    printf("\nfailed in computer science");
+    }
+
+     
+}
